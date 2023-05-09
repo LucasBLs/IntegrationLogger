e568a5b0226c0ed7f274eb9e563aab4b17404c58
LucasBLs <lucasblemos@hotmail.com>
2023-05-08 22:21:21 -0300

    ok

--- Controllers/IntegrationLogController.cs
+++ Controllers/IntegrationLogController.cs
@@ -1,3 +1,6 @@
+using IntegrationLogger.Extensions;
+using IntegrationLogger.Models;
+using IntegrationLogger.Models.Integration;
 using IntegrationLogger.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,16 +10,29 @@ namespace IntegrationLogger.Controllers;
 [Route("api/[controller]")]
 public class IntegrationLogController : ControllerBase
 {
-    private readonly IIntegrationLogRepository _integrationLogRepository;
-    public IntegrationLogController(IIntegrationLogRepository integrationLogRepository)
+    [HttpGet("get-logs")]
+    public async Task<IActionResult> GetLogs(
+        [FromServices] IIntegrationLogRepository repository,
+        DateTimeOffset? startDate = null,
+        DateTimeOffset? endDate = null,
+        string? integrationName = null,
+        string? externalSystem = null,
+        string? sourceSystem = null,
+        bool groupByIntegrationName = false,
+        int pageIndex = 1,
+        int pageSize = 0)
     {
-        _integrationLogRepository = integrationLogRepository;
-    }
-
-    [HttpGet("GetAll")]
-    public async Task<IActionResult> GetAll()
-    {
-        var result = await _integrationLogRepository.GetLogs();
-        return Ok(result);
+        if (!ModelState.IsValid)
+            return BadRequest(new ApiResponse<List<IntegrationLog>>(false, "ModelState Inválido.", null, ModelState.GetErrors()));
+        try
+        {
+            var result = await repository.GetLogs(startDate, endDate, integrationName, externalSystem, sourceSystem, groupByIntegrationName, pageIndex, pageSize);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            var response = new ApiResponse<List<IntegrationLog>>(false, ex.Message, null, ex);
+            return BadRequest(response);
+        }
     }
 }
